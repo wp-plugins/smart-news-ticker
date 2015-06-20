90053cee53789f970c27a47423960555511c05a5
yash007 <yash007@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-20 04:31:33 +0000

    tiny mc added
    git-svn-id: https://plugins.svn.wordpress.org/smart-news-ticker/trunk@1184557 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/ticker-custom-button.js
+++ js/ticker-custom-button.js
@@ -1,12 +1,84 @@
 
+
 (function() {
 	tinymce.PluginManager.add('tp_ticker_button', function( editor, url ) {
 		editor.addButton( 'tp_ticker_button', {
-			icon: 'sullu',
-			title : 'Smart Ticker',
-			onclick: function() {
-				editor.insertContent('[tp_smart_ticker]');
-			}
+			icon: 'chukku',
+			type: 'menubutton',
+			title : 'News Ticker',
+					menu: [
+						{
+							text: 'News Ticker',
+							onclick: function() {
+								editor.windowManager.open( {
+									title: 'Insert Ticker Shortcode',
+									width: 600,
+									height: 335,
+									body: [
+										{
+											type: 'textbox',
+											name: 'tickerID',
+											label: 'Ticker ID',
+											value: 'Write_Anything'
+											
+										},
+										{
+											type: 'textbox',
+											name: 'tickerTitle',
+											label: 'Ticker Text',
+											value: 'Latest News'
+											
+										},
+										{
+											type: 'textbox',
+											name: 'TickertitleBg',
+											label: 'Title Background',
+											value: '#333333'
+											
+										},
+										{
+											type: 'listbox',
+											name: 'Tickereffect',
+											label: 'Ticker Effect',
+											'values': [
+												{text: 'Slide', value: 'slide'},
+												{text: 'Fade', value: 'fade'}
+											]
+										},
+										{
+											type: 'textbox',
+											name: 'TickerTextColor',
+											label: 'Ticker Text Color',
+											value: '#00000'
+											
+										},
+										{
+											type: 'textbox',
+											name: 'TickerfontSize',
+											label: 'Font Size',
+											value: '13'
+											
+										},
+										{
+											type: 'textbox',
+											name: 'tickerBgColor',
+											label: 'Ticker BG Color'
+											
+										},	
+										{
+											type: 'textbox',
+											name: 'tickerCategory',
+											label: 'Category'
+										}										
+									],
+									onsubmit: function( e ) {
+										editor.insertContent( '[tp_smart_ticker id="' + e.data.tickerID + '" text="' + e.data.tickerTitle + '" textbg="' + e.data.TickertitleBg + '" effect="' + e.data.Tickereffect + '" color="' + e.data.TickerTextColor + '" ticker_bg="' + e.data.tickerBgColor + '" category="' + e.data.tickerCategory + '" fontsize="' + e.data.TickerfontSize + '"]');
+									}
+								});
+							}
+						}
+					]
 		});
 	});
 })();
+
